d95b27c8069cd86ac645ca9eee15659e1cb79057
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2024-04-24 09:03:22 +0100

    Started assignment two

---
+++ assign2/src/TimeServer.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+ 
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java TimeServer <port>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        try (
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        ) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread.ofVirtual().start(() -> {
+                    try (
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    ) {
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            System.out.println(inputLine);
+                            out.println(inputLine);
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
